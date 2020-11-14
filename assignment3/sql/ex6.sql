c2cb343da88baa79d48a003fbd19224acd1b96b7
Aiden <12aduffy@gmail.com>
2020-11-14 14:46:18 -0500

    [Feat] Completed question 6
    + Added SQL code for question 6 to have interesting data modification statemetns

--- assignment3/sql/ex6.sql
+++ assignment3/sql/ex6.sql
@@ -1,1 +1,15 @@
 USE se3309;
+
+DELETE 
+FROM UserComment
+WHERE likeCount <= 0 AND parentCommentId > 0;
+
+INSERT INTO Friendship
+VALUES ("aglae34", 
+	(SELECT author
+    FROM Review
+    WHERE title = "Interstellar"));
+    
+UPDATE Review
+SET numericalRating = 1
+WHERE numericalRating = 0 AND reviewNumber > 0;
--- assignment3/sql/test.sql
+++ assignment3/sql/test.sql
@@ -1,12 +1,14 @@
 USE se3309; 
-/*
-SELECT DISTINCT u.username, u.likeCount 
-FROM MovieListUser u, Review r, UserComment c, WatchListEntry w
-WHERE u.likeCount > 1000000 AND u.username != r.author AND u.username != c.author AND u.username != w.username; */
 
-SELECT username, likeCount 
-FROM MovieListUser u, Review r 
-WHERE likeCount > 100000 AND
-	(SELECT COUNT(author) AS reviewCount
-    FROM Review
-    GROUP BY author) = 0;
+
+
+UPDATE Review
+SET numericalRating = 1
+WHERE numericalRating = 0 AND reviewNumber > 0;
+
+
+/*
+SELECT reviewNumber, author, title, numericalRating
+FROM Review
+ORDER BY numericalRating;*/
+
